a533b6b0cd42cd2ee7525faba0b42a32daf2edc1
APauquin <111913188+APauquin@users.noreply.github.com>
2023-03-21 00:25:33 +0100

    fixed typos

--- info.php
+++ info.php
@@ -12,19 +12,20 @@
     
     <?php	
         $nom = $_POST['user_name'];
-        $firstname = $_POST['user_firstname'];
         $courriel = $_POST['user_email'];
         $phone = $_POST['user_phoneNumber'];
-        $sujet = $_POST['object-message'];
+        $sujet = $_POST['user_sujet'];
         $message = $_POST['user_message'];
     ?>
     
-    <p> Hello <?= $firstname ?>, you suck. You contacted us to talk about <?= $sujet ?>.
+    <p> Hello <?= $nom ?>, you suck. You contacted us to talk about <?= $sujet ?>.
 
     We will not be contacting you and have sent your email: "<?= $courriel ?>" and phone number: "<?= $phone ?>" to shady supplement companies so they may bombard you with ads. Screw you! </p> <br>
     
     <p> PS: We really dont care about the message you wrote us! </p>
+    
     <p> <?= $message ?> </p>
+    
 </body>
 
 </html>
