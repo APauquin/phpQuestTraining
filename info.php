e10ccc2e54f8c9a3526ffded25f0ff3a79f1e78e
APauquin <alexandrep123@yahoo.com>
2023-03-21 14:56:51 +0100

    deleted useless code

--- info.php
+++ info.php
@@ -11,24 +11,14 @@
 
 <body>
 
+    <p> Hello <?= $_POST['user_firstname'] . ' ' . $_POST['user_lastname'];?>, you suck. You contacted us to talk about <?= $_POST['user_sujet'] ?>.
 
 
-    <?php	
-        $name = $_POST['user_firstname'] . ' ' . $_POST['user_lastname'];
-        $courriel = $_POST['user_email'];
-        $phone = $_POST['user_phoneNumber'];
-        $sujet = $_POST['user_sujet'];
-        $message = $_POST['user_message'];
-    ?>
-
-    <p> Hello <?= $name?>, you suck. You contacted us to talk about <?= $sujet ?>.
-
-
-    We will not be contacting you and have sent your email: "<?= $courriel ?>" and phone number: "<?= $phone ?>" to shady supplement companies so they may bombard you with ads. Screw you! </p> <br>
+    We will not be contacting you and have sent your email: "<?= $_POST['user_email']?>" and phone number: "<?= $_POST['user_phoneNumber'] ?>" to shady supplement companies so they may bombard you with ads. Screw you! </p> <br>
     
     <p> PS: We really dont care about the message you wrote us! </p>
     
-    <p> <?= $message ?> </p>
+    <p> <?= $_POST['user_message'] ?> </p>
     
 </body>
 
